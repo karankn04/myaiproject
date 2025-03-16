0bd1c9e269a6c24d4d1bc0ba6b4d7c6ab2f47f2c
karan <karan.singh.Kn04@gmail.com>
2025-03-16 12:30:37 +0530

    add Todo component and input field to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Todo from "@/ui/Todo";
 
 
 
@@ -5,6 +6,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       this is my first project my chsnhrsdfsd sd f sdf sd f
+      <div>
+     <input type="text" />
+     <button>Add this</button >
+    </div>
     </div>
   );
 }
---
+++ ui/Todo.tsx
@@ -0,0 +1,15 @@
+const Todo = () => {
+
+
+
+  return (
+     <div>
+     <input type="text" />
+     <button>Add Todo</button>
+    </div>
+  )
+
+}
+   
+
+export default Todo;
